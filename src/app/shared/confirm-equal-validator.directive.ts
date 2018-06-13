84c39461b6bf0a9a350ddde61eed3ea4b27421dd
Mathew Fuller <mathewfuller@Mathews-MacBook-Pro.local>
2018-06-13 10:33:30 -0600

    video 27 commit

---
+++ src/app/shared/confirm-equal-validator.directive.ts
@@ -0,0 +1,14 @@
+import { Validator, NG_VALIDATORS } from '@angular/forms';
+import { Directive } from '@angular/core';
+
+@Directive({
+    selector: '[appConfirmEqualValidator]',
+    providers: [{
+        provide: NG_VALIDATORS,
+        useExisting: ConfirmEqualValidatorDirective,
+        multi: true
+    }]
+})
+export class ConfirmEqualValidatorDirective implements Validator {
+
+}
