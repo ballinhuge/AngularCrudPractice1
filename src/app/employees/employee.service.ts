51333148dd51577df18e27ee6c06e7a63f71d10e
Mathew Fuller <mathewjfulller@outlook.com>
2018-06-13 11:23:02 -0600

    video 30 commit

--- src/app/employees/employee.service.ts
+++ src/app/employees/employee.service.ts
@@ -1,9 +1,44 @@
 import { Injectable } from '@angular/core';
+import { Employee } from '../models/employee.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
+  private listEmployees: Employee[] = [
+    {
+      id: 1,
+      name: 'Mark',
+      gender: 'Male',
+      contactPreference: 'Email',
+      email: 'mark@pragimtech.com',
+      dateOfBirth: new Date('10/25/1988'),
+      department: 'IT',
+      isActive: true
+    },
+    {
+      id: 1,
+      name: 'Mary',
+      gender: 'Female',
+      contactPreference: 'Phone',
+      phoneNumber: 2345678908,
+      dateOfBirth: new Date('11/20/1979'),
+      department: 'HR',
+      isActive: true
+    },
+    {
+      id: 3,
+      name: 'John',
+      gender: 'Male',
+      contactPreference: 'Phone',
+      phoneNumber: 54723645726,
+      dateOfBirth: new Date('3/25/1976'),
+      department: 'IT',
+      isActive: false,
+    }
+  ];
 
-  constructor() { }
+  getEmployees(): Employee[] {
+    return this.listEmployees;
+  }
 }
--- src/app/employees/list-employees.component.ts
+++ src/app/employees/list-employees.component.ts
@@ -1,47 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { Employee } from '../models/employee.model';
+import { EmployeeService } from './employee.service';
 
 @Component({
   templateUrl: './list-employees.component.html',
   styleUrls: ['./list-employees.component.css']
 })
 export class ListEmployeesComponent implements OnInit {
-  employees: Employee[] = [
-    {
-      id: 1,
-      name: 'Mark',
-      gender: 'Male',
-      contactPreference: 'Email',
-      email: 'mark@pragimtech.com',
-      dateOfBirth: new Date('10/25/1988'),
-      department: 'IT',
-      isActive: true
-    },
-    {
-      id: 1,
-      name: 'Mary',
-      gender: 'Female',
-      contactPreference: 'Phone',
-      phoneNumber: 2345678908,
-      dateOfBirth: new Date('11/20/1979'),
-      department: 'HR',
-      isActive: true
-    },
-    {
-      id: 3,
-      name: 'John',
-      gender: 'Male',
-      contactPreference: 'Phone',
-      phoneNumber: 54723645726,
-      dateOfBirth: new Date('3/25/1976'),
-      department: 'IT',
-      isActive: false,
-    }
-  ];
-  constructor() { }
+  employees: Employee[];
+  constructor(private _employeeService: EmployeeService) { }
 
   ngOnInit() {
-
+    this.employees =
+      this._employeeService.getEmployees();
   }
 
 }
